3f01d4257f9739a6a50da3656ba19021ee1e28c4
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-02-26 21:56:16 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 // import { useState } from 'react';
 // import './App.css';
 // import Button from "./components/UI/Button/Button";
@@ -92,10 +91,10 @@
 
 //   if(startFlag)
 //   {
-//     qCards = 
+//     qCards =
 //     <div>
 //       {
-//         questionData.map((data) => 
+//         questionData.map((data) =>
 //           <Card question= {data.question} options = {{
 //             option1 : data.options.option1,
 //             option2 : data.options.option2,
@@ -103,7 +102,7 @@
 //             option4 : data.options.option4,
 //           }} > </Card>)
 //       }
-//     </div> 
+//     </div>
 
 //     startBtn = <></>;
 //   }
@@ -111,7 +110,7 @@
 //   {
 //     startBtn = <Button onClick = {handleStart}>Start Quiz</Button>;
 //   }
-  
+
 //   return (
 //     <div className="App">
 //       <h1>
@@ -219,10 +218,10 @@
 
 //   if(startFlag)
 //   {
-//     qCards = 
+//     qCards =
 //     <div>
 //       {
-//         questionData.map((data) => 
+//         questionData.map((data) =>
 //           <Card question= {data.question} options = {{
 //             option1 : data.options.option1,
 //             option2 : data.options.option2,
@@ -230,7 +229,7 @@
 //             option4 : data.options.option4,
 //           }} > </Card>)
 //       }
-//     </div> 
+//     </div>
 
 //     startBtn = <></>;
 //     resultBtn = <Button onClick={() => setResultFlag(true)}>Show Results</Button>
@@ -240,7 +239,7 @@
 //     startBtn = <Button onClick={handleStart}>Start Quiz</Button>;
 //     resultBtn = <></>;
 //   }
-  
+
 //   return (
 //     <div className="App">
 //       <h1>
@@ -255,139 +254,143 @@
 // }
 
 // export default App;
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
 import Banner from "./components/UI/Banner/Banner";
-import React from 'react';
+import React from "react";
 
 function App() {
-
   var qCards;
   var startBtn;
   var resultBtn;
 
   const questionData = [
     {
-      key:1,
-      question:'Who is the father of the nation ?',
+      key: 1,
+      question: "Who is the father of the nation ?",
       //correctAnswerMarkUpdate,
       //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
+      options: {
+        option1: "Mahatma Gandhi",
+        option2: "Jawaharlal Nehru",
+        option3: "Donald Trump",
+        option4: "Barrack Obama",
       },
-      answer : 'Mahatma Gandhi'
+      answer: "Mahatma Gandhi",
     },
     {
-      key : 2,
-      question : "What color is are the leaves ?",
+      key: 2,
+      question: "What color is are the leaves ?",
       //correctAnswerMarkUpdate,
       //attempt,
-      options : {
-        option1 : "Blue",
-        option2 : "Red",
-        option3 : "Yellow",
-        option4 : "Green",
+      options: {
+        option1: "Blue",
+        option2: "Red",
+        option3: "Yellow",
+        option4: "Green",
       },
-      answer : "Green"
+      answer: "Green",
     },
     {
-      key:3,
-      question:'Who is the father of the nation ?',
+      key: 3,
+      question: "Who is the father of the nation ?",
       //correctAnswerMarkUpdate,
       //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
+      options: {
+        option1: "Mahatma Gandhi",
+        option2: "Jawaharlal Nehru",
+        option3: "Donald Trump",
+        option4: "Barrack Obama",
       },
-      answer : 'Mahatma Gandhi'
+      answer: "Mahatma Gandhi",
     },
     {
-      key:4,
-      question:'Who is the father of the nation ?',
+      key: 4,
+      question: "Who is the father of the nation ?",
       //correctAnswerMarkUpdate,
       //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
+      options: {
+        option1: "Mahatma Gandhi",
+        option2: "Jawaharlal Nehru",
+        option3: "Donald Trump",
+        option4: "Barrack Obama",
       },
-      answer : 'Mahatma Gandhi'
+      answer: "Mahatma Gandhi",
     },
     {
-      key:5,
-      question:'Who is the father of the nation ?',
+      key: 5,
+      question: "Who is the father of the nation ?",
       //correctAnswerMarkUpdate,
       //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
+      options: {
+        option1: "Mahatma Gandhi",
+        option2: "Jawaharlal Nehru",
+        option3: "Donald Trump",
+        option4: "Barrack Obama",
       },
-      answer : 'Mahatma Gandhi'
+      answer: "Mahatma Gandhi",
     },
-  ]
+  ];
   const [startFlag, setStartFlag] = useState(false);
   const [resultFlag, setResultFlag] = useState(false);
-  const [buttonText, setButtonText] = useState('Start Quiz');
+  const [buttonText, setButtonText] = useState("Start Quiz");
   const [questionsCorrect, setQuestionsCorrect] = useState(0);
   const [answered, setAnswered] = useState(0);
 
-  function handleStart()
-  {
+  function handleStart() {
     setStartFlag(true);
-    setButtonText('Started');
+    setButtonText("Started");
   }
 
   function handleOptionClick(isCorrect) {
+    console.log(isCorrect);
     setAnswered(answered + 1);
     if (isCorrect) {
       setQuestionsCorrect(questionsCorrect + 1);
     }
   }
 
-  if(startFlag)
-  {
-    qCards = 
-    <div>
-      {
-        questionData.map((data) => 
-          <Card key={data.key} question= {data.question} options = {{
-            option1 : data.options.option1,
-            option2 : data.options.option2,
-            option3 : data.options.option3,
-            option4 : data.options.option4,
-          }} onOptionClick={handleOptionClick}
-          correctAnswerMarkUpdate={(flag) => handleOptionClick(flag)}> </Card>)
-      }
-    </div> 
+  if (startFlag) {
+    qCards = (
+      <div>
+        {questionData.map((data) => (
+          <Card
+            key={data.key}
+            question={data.question}
+            options={{
+              option1: data.options.option1,
+              option2: data.options.option2,
+              option3: data.options.option3,
+              option4: data.options.option4,
+            }}
+            answer={data.answer}
+            onOptionClick={handleOptionClick}
+            correctAnswerMarkUpdate={(flag) => handleOptionClick(flag)}
+          >
+            {" "}
+          </Card>
+        ))}
+      </div>
+    );
 
     startBtn = <></>;
-    resultBtn = <Button onClick={() => setResultFlag(true)}>Show Results</Button>
-  }
-  else
-  {
+    resultBtn = (
+      <Button onClick={() => setResultFlag(true)}>Show Results</Button>
+    );
+  } else {
     startBtn = <Button onClick={handleStart}>Start Quiz</Button>;
     resultBtn = <></>;
   }
-  
+
   return (
     <div className="App">
-      <h1>
-        Quizz App
-      </h1>
+      <h1>Quizz App</h1>
       {qCards}
       {startBtn}
       {resultBtn}
-      {resultFlag && <Banner score={questionsCorrect}/>}
+      {resultFlag && <Banner score={questionsCorrect} />}
     </div>
   );
 }
--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,30 +1,37 @@
-import React, { useState } from 'react';
-import Button from '../Button/Button';
-const Card = ({key,
-    question,
-    correctAnswerMarkUpdate,
-    attempt,
-    options,
-    answer}) => {
-        const [state, setSate] = useState(false);
-        function disableBtn()
-        {
-          correctAnswerMarkUpdate(answer===options.option)
-            setSate(true);
-        }
+import React, { useState } from "react";
+import Button from "../Button/Button";
+const Card = ({
+  key,
+  question,
+  correctAnswerMarkUpdate,
+  attempt,
+  options,
+  answer,
+}) => {
+  const [state, setSate] = useState(false);
 
+  function disableBtn(option) {
+    correctAnswerMarkUpdate(answer === option);
+    setSate(true);
+  }
 
-        return (
-            <div>
-            <h4>
-                {question}
-            </h4>
-            <Button onClick={() => disableBtn(options.option)} disabled = {state} >{options.option1}</Button>
-            <Button onClick={() => disableBtn(options.option)} disabled = {state} >{options.option2}</Button>
-            <Button onClick={() => disableBtn(options.option)} disabled = {state} >{options.option3}</Button>
-            <Button onClick={() => disableBtn(options.option)} disabled = {state} >{options.option4}</Button>
-            </div>
-        )
-    }
+  return (
+    <div>
+      <h4>{question}</h4>
+      <Button onClick={() => disableBtn(options.option1)} disabled={state}>
+        {options.option1}
+      </Button>
+      <Button onClick={() => disableBtn(options.option2)} disabled={state}>
+        {options.option2}
+      </Button>
+      <Button onClick={() => disableBtn(options.option3)} disabled={state}>
+        {options.option3}
+      </Button>
+      <Button onClick={() => disableBtn(options.option4)} disabled={state}>
+        {options.option4}
+      </Button>
+    </div>
+  );
+};
 
-    export default Card;
+export default Card;
